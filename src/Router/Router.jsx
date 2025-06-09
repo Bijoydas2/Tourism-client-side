16b3dd3ec9ba1b9385ddabf9b473bc5c06e03803
bijoy133 <bijoydas5680@!gmail.com>
2025-06-09 10:29:00 +0600

    setup client side vite,tailwind,daisyui,react router

---
+++ src/Router/Router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter,} from "react-router";
+
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-7xl ">Hello World</div>,
+  },
+]);
