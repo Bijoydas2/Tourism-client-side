a49c3b280fb4bd8260b7fe62a5b12874726ff73b
bijoy133 <bijoydas5680@!gmail.com>
2025-08-10 11:38:05 +0600

    Update package feature with improved functionality

--- src/Page/Home/FeaturedPackages.jsx
+++ src/Page/Home/FeaturedPackages.jsx
@@ -9,7 +9,7 @@ const FeaturedPackages = ({ feauturedPackagesPromise }) => {
     <section className=" mt-16 max-w-7xl mx-auto">
       <h2 className="text-4xl font-semibold mb-8 text-center text-blue-700">Featured Packages</h2>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {packages.map(pkg => (
           <FeaturedCard key={pkg._id} package={pkg} />
         ))}
